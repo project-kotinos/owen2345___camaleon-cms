b32caf07146734b898f9c1a57326693e309efcc9
Keheliya Gallaba <keheliya.gallaba@gmail.com>
2019-08-06 21:51:28 -0400

    Update yourbased.sh

--- yourbased.sh
+++ yourbased.sh
@@ -4,9 +4,11 @@ set -ex
 export DEBIAN_FRONTEND=noninteractive
 export BUNDLE_GEMFILE=$PWD/gemfiles/rails_6_0.gemfile
 
+
+apt-get install gdebi-core
 export CHROME_SOURCE_URL=https://dl.google.com/dl/linux/direct/google-chrome-stable_current_amd64.deb
 wget --no-verbose -O /tmp/$(basename $CHROME_SOURCE_URL) $CHROME_SOURCE_URL
-dpkg -i /tmp/$(basename $CHROME_SOURCE_URL)
+gdebi --n /tmp/$(basename $CHROME_SOURCE_URL)
 
 #before install
 echo 'gem: --no-document' > ~/.gemrc
